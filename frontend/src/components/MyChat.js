c68c89d21f7b805a79b12cc9aae93118a5b417be
krushang-07 <krushangsavaliya45@gmail.com>
2024-05-06 15:59:42 +0530

    new pb

---
+++ frontend/src/components/MyChat.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MyChat = () => {
+  return (
+    <div>
+      my chats
+    </div>
+  )
+}
+
+export default MyChat
--- frontend/src/components/other/SideDrawer.js
+++ frontend/src/components/other/SideDrawer.js
@@ -1,16 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
+import { Box } from "@chakra-ui/layout";
+import { Tooltip } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/button";
+import { Text } from "@chakra-ui/react";
+import { BellIcon, ChevronDownIcon } from "@chakra-ui/icons";
+import { Avatar } from "@chakra-ui/avatar";
+import { ChatState } from "../../Context/ChatProvider";
+import { Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/menu";
+import ProfileModel from "./ProfileModel";
+//import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 const SideDrawer = () => {
+  const [search, setSearch] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [loadingchat, setLoadingChat] = useState();
+
+  const { user } = ChatState();
+  // const history = useHistory();
+  // const logoutHandler = () => {
+  //   JSON.parse(localStorage.removeItem("userInfo"));
+  //   history.push("/");
+  // };
   return (
-    <div
-      style={{
-        width: "50%",
-        fontSize: "100px",
-        color: "red",
-      }}
-    >
-      Hello World
-    </div>
+    <>
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+        bg="white"
+        w="100%"
+        p="5px 10px 5px 10px"
+        borderWidth="5px"
+      >
+        <Tooltip label="search users to chat" hasArrow placement="bottom-end">
+          <Button variant="ghost">
+            <i class="fas fa-search"></i>
+            <Text d={{ base: "none", md: "flex" }} px="4">
+              Search User
+            </Text>
+          </Button>
+        </Tooltip>
+        <Text fontSize="2xl" fontFamily="work sans" color="black">
+          Chatter-Space
+        </Text>
+        <div>
+          <Menu>
+            <MenuButton p={1}>
+              <BellIcon fontSize="2xl" m={1} />
+            </MenuButton>
+          </Menu>
+          <Menu>
+            <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+              <Avatar
+                size="sm"
+                cursor="pointer"
+                name={user.name}
+                src={user.pic}
+              />
+            </MenuButton>
+            <MenuList>
+              <ProfileModel user={user}>
+                <MenuItem>My Profile</MenuItem>
+              </ProfileModel>
+              <MenuItem>Logout</MenuItem>
+            </MenuList>
+          </Menu>
+        </div>
+      </Box>
+    </>
   );
 };
 
