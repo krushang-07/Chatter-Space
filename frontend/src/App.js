a118dac3b83aca45de2f783ca0d21a46ada2d6a2
krushang-07 <krushangsavaliya45@gmail.com>
2024-05-22 15:35:09 +0530

    d

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,14 @@
 import "./App.css";
 import { Route } from "react-router-dom";
 import ChatPage from "./Pages/ChatPage";
-import Homepage from "./Pages/Homepage";
+import HomePage from "./Pages/HomePage";
 import ChatProvider from "./Context/ChatProvider";
 
 function App() {
   return (
     <ChatProvider>
       <div className="App">
-        <Route path="/" component={Homepage} exact />
+        <Route path="/" component={HomePage} exact />
         <Route path="/chats" component={ChatPage} />
       </div>
     </ChatProvider>
